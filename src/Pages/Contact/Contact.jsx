62b7d9276c50cf0c52b0b2c9036b4d72bfc6fa65
neamat-2024 <neamathshuvon8@gmail.com>
2024-05-20 15:22:57 +0600

    update logic

--- src/Pages/Contact/Contact.jsx
+++ src/Pages/Contact/Contact.jsx
@@ -1,4 +1,6 @@
 import React,{useState} from 'react'
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
 import Navbar from '../../Component/Navbar'
 import Footer from '../../Component/Footer'
 import Datepicker from '../../Component/Datepicker'
@@ -11,7 +13,7 @@ const Contact = () => {
   const[to,selectto]=useState("");
 
   const handlesubmit =(e)=>{e.preventDefault();
-    setSearchParams({Name: name,FromCity:from, TOCity: to });
+    setSearchParams({Name: name,OriginAirport:from, DestinationAirport: to });
 
   }
   //const{ userid }=useParams();
@@ -22,34 +24,48 @@ const Contact = () => {
         <div className='mt-4'>
         <h1>This is Contactpage</h1>
         <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minima libero, laboriosam maiores iure harum architecto saepe porro quos molestias ea illo eos, doloribus nihil neque eius iste id quisquam voluptas.</p>
-        user
         <div className='mb-5'>
-          <h1>Username</h1>
-          <form onSubmit={handlesubmit}>
-            <input type="text" value={name} placeholder='Enter name' onChange={(e)=>{setname(e.target.value)}}/>
-          </form>
-          <form onSubmit={handlesubmit} className='mt-3'>
-            <div className='mt-3 mb-3'><label for="Select from City">From:</label>
-              <select onChange={handlesubmit} value={selectfrom} onChange={(e)=>{selectfrom(e.target.value)}} id="fromcity" name="carlist" form="carform" >
-                <option value="dhaka">Dhaka</option>
-                <option value="chittagong">Chittagong</option>
-                <option value="sylhet">Sylhet</option>
-                <option value="cox'sbajar">Cox's Bajar</option>
-              </select>
-            </div>
-          </form> 
-          <form onSubmit={handlesubmit} className='mt-3'>
-            <div className='mt-3 mb-3'><label for="Select to City">To:</label>
-              <select onChange={handlesubmit} value={selectto} onChange={(e)=>{selectto(e.target.value)}} id="tocity" name="carlist" form="carform" >
-                <option value="dhaka">Dhaka</option>
-                <option value="chittagong">Chittagong</option>
-                <option value="sylhet">Sylhet</option>
-                <option value="cox'sbajar">Cox's Bajar</option>
-              </select>
-            </div>
-            <button type="submit" target="blank" className=''>Submit</button>
-          </form> 
+          <div className=" align-items-center">
+            <div className="col-md-4">
+              
+              {/* <form onSubmit={handlesubmit}>
+                <label for="name" className='me-3'>Enter Name :</label>
+                <input type="text" value={name} placeholder='Enter name' onChange={(e)=>{setname(e.target.value)}}/>
+              </form> */}
 
+              <Form onSubmit={handlesubmit} className='mt-4'>
+                <Form.Label>Enter Your Name</Form.Label>
+                <Form.Control type="name" size="lg" placeholder="Enter Name" value={name} onChange={(e)=>{setname(e.target.value)}} />
+              </Form>
+
+              <Form onSubmit={handlesubmit} className='mt-4'>
+                <Form.Label>Select Origin Airport</Form.Label>
+                  <Form.Select size="lg" onSubmit={handlesubmit} className='' value={from} onChange={(e)=>{selectfrom(e.target.value)}}>
+                      <option>Select Airport</option>
+                      <option value="dhaka">Dhaka</option>
+                      <option value="chittagong">Chittagong</option>
+                      <option value="sylhet">Sylhet</option>
+                      <option value="doha">Doha</option>
+                      <option value="saidpur">Saidpur</option>
+                  </Form.Select>
+              </Form>
+
+              <Form onSubmit={handlesubmit} className='mt-4'>
+                <Form.Label>Select Your Destination</Form.Label>
+                  <Form.Select size="lg" onSubmit={handlesubmit} className='' value={to} onChange={(e)=>{selectto(e.target.value)}}>
+                      <option>Select Airport</option>
+                      <option value="dhaka">Dhaka</option>
+                      <option value="chittagong">Chittagong</option>
+                      <option value="sylhet">Sylhet</option>
+                      <option value="doha">Doha</option>
+                      <option value="saidpur">Saidpur</option>
+                  </Form.Select>
+                <Button variant="primary" type="submit" className='mt-4 text-end'>Submit</Button>
+              </Form>
+              
+            </div>
+          </div>
+          
         </div>
         {/* <h1>{searchParams.get("id")}</h1>
         <h1>{searchParams.get("age")}</h1>
